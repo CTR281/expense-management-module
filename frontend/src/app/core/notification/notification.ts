dbdc3e1d966ba46f0eeb235829f9465202f29cf0
CTR281 <chmartin2.81@gmail.com>
2026-02-05 08:52:43 +0100

    :sparkles: enable closing notifications

--- frontend/src/app/core/notification/notification.ts
+++ frontend/src/app/core/notification/notification.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from "@angular/core";
 import { NotificationService } from "./notification-service";
+import { IconCloseComponent } from "../../shared/ui/icons/icon-close";
 
 @Component({
   selector: "app-notification",
-  imports: [],
+  imports: [IconCloseComponent],
   templateUrl: "./notification.html",
   styleUrl: "./notification.css",
 })
---
+++ frontend/src/app/shared/ui/icons/icon-close.ts
@@ -0,0 +1,27 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-icon-close",
+  template: ` <svg
+    width="20px"
+    height="20px"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="24" height="24" fill="none" />
+    <path
+      d="M7 17L16.8995 7.10051"
+      stroke="#ffffff"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M7 7.00001L16.8995 16.8995"
+      stroke="#ffffff"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>`,
+})
+export class IconCloseComponent {}
