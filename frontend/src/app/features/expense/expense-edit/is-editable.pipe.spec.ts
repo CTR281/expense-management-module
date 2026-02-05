5b67581fed25d501f3cd2fdd64135815d34e290d
CTR281 <chmartin2.81@gmail.com>
2026-02-05 13:46:41 +0100

    :fire: remove dummy tests

--- frontend/src/app/features/expense/expense-edit/is-editable.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IsEditablePipe } from "./is-editable.pipe";
-
-describe("IsEditablePipe", () => {
-  it("create an instance", () => {
-    const pipe = new IsEditablePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- frontend/src/app/shared/util/not-empty-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NotEmptyPipe } from "./not-empty-pipe";
-
-describe("NotPipe", () => {
-  it("create an instance", () => {
-    const pipe = new NotEmptyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
